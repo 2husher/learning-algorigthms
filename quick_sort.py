14e5c7e685d0f29938ab873407f83b09df0cebc4
Alex Izotov <izotovalexander@gmail.com>
2023-05-15 11:07:16 +0300

    Update a quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,19 +1,7 @@
 def quick_sort(sequence):
     if len(sequence) < 2: return sequence
 
-    middle_index = len(sequence) // 2
-    middle_elem = sequence[middle_index]
-
-    left = []
-    middle = []
-    right = []
-    for elem in sequence:
-        if elem < middle_elem:
-            left.append(elem)
-        elif elem > middle_elem:
-            right.append(elem)
-        else:
-            middle.append(elem)
+    left, middle, right = divide(sequence)      # n
 
     sorted_left = quick_sort(left)
     sorted_right = quick_sort(right)
@@ -22,15 +10,39 @@ def quick_sort(sequence):
     return sorted_sequence
 
 
+def divide(sequence):
+    middle_index = len(sequence) // 2
+    middle_elem = sequence[middle_index]
+
+    left = []
+    middle = []
+    right = []
+    for elem in sequence:       # n
+        if elem < middle_elem:
+            left.append(elem)
+        elif elem > middle_elem:
+            right.append(elem)
+        else:
+            middle.append(elem)
+    return left, middle, right
+
+
 import random
 
 
 def main():
-    n = 50
+    n = 30
     start_sequence = []
     for i in range(0, n):
         start_sequence.append(random.randint(0, 10000))
 
-    assert quick_sort(start_sequence) == sorted(start_sequence)
+    checked = sorted(start_sequence.copy())
+
+    quick_sorted = quick_sort(start_sequence.copy())
+
+    # print(f"QUICK SORTED: {quick_sorted}")
+
+    assert quick_sorted == checked
+
 
 main()
